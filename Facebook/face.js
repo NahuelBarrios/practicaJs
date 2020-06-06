f7d372299fb7d17ea5291a5253404641e4a7d99f
NahuelBarrios <retro_io@hotmail.com>
2020-06-05 22:29:43 -0300

    Iniciar sesion facebook

---
+++ Facebook/face.js
@@ -0,0 +1,13 @@
+function entrar ()
+{
+	var email = 0,contraseña = 0;
+
+	email = document.getElementById("email").value;
+
+	contraseña = document.getElementById("contraseña").value; 
+
+
+	document.write("su email es: "+email+" y su contraseña es: "+ contraseña +"<br>");
+
+
+}
