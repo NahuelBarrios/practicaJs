0d310f5b47b3b89762570fe338686730da543adb
NahuelBarrios <52472582+NahuelBarrios@users.noreply.github.com>
2020-07-24 23:38:46 -0300

    Add files via upload
    pagina Restourant complete!

---
+++ paginaRestourant/js/menu.js
@@ -0,0 +1,10 @@
+var btnMenu = document.getElementById("btnmenu");
+var menu = document.getElementById("menu");
+
+btnMenu.addEventListener('click',function(){
+'use strict';
+
+menu.classList.toggle('mostrar');
+
+
+});
